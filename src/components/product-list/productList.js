2fb6038dc6a1d87c558c56d80c637fd30c7751bd
recito prasidha <recitogigih9@gmail.com>
2021-09-08 14:29:46 +0700

    alt image fixed

--- src/components/product-list/productList.js
+++ src/components/product-list/productList.js
@@ -8,7 +8,7 @@ class productList extends Component {
     return (
     <div className="col-3-desktop">
       <div className="card"> 
-        <img className="fullwidth" src={this.props.imageUrl} alt="Denim Jeans"/>
+        <img className="fullwidth" src={this.props.imageUrl} alt={this.props.name}/>
         <h5> {this.props.name.length > 30 ?`${this.props.name.substring(0, 30)}...` : this.props.name}</h5>
         <del className="strikethrough">${this.props.retailPrice}</del>
         <p className="price">${this.props.salePrice}</p>
